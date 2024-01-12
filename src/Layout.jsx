7481ce2c080dfe5ca508f12aa03e7c3fd58865b2
TheTechSage <103244076+TheTechSage@users.noreply.github.com>
2024-01-12 16:04:59 +0530

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer";
 
 export default function Layout() {
     return (
